8e05e9f265f08bf8fd0c3f2b488c108077c97568
Dan Stone <wotbrew@gmail.com>
2022-01-16 12:51:24 +0000

    bump snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.wotbrew/relic "0.1.1"
+(defproject com.wotbrew/relic "0.1.2-SNAPSHOT"
   :description "Functional relational programming for Clojure/Script"
   :url "https://github.com/wotbrew/relic"
   :license {:name "MIT"
